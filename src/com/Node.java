35b27427fdf49c8d41b17977facaf3a4d854f4ca
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-08 22:56:03 +1000

    Added javadoc comments to the node class

--- src/com/Node.java
+++ src/com/Node.java
@@ -4,6 +4,10 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Generic node in a search space.
+ * @param <T> - type of action performed (likely an enumeration)
+ */
 public class Node<T> implements Comparable<Node<T>> {
 	private final Node<T> parent;
 	private final State<T> state;
@@ -12,6 +16,10 @@ public class Node<T> implements Comparable<Node<T>> {
 	private final int pcost;
 	private final int hcost;
 	
+	/**
+	 * Construct a node from an initial state in the search space.
+	 * @param initState - initial state
+	 */
 	public Node(State<T> initState) {
 		parent = null;
 		action = null;
@@ -20,6 +28,12 @@ public class Node<T> implements Comparable<Node<T>> {
 		hcost  = 0;
 	}
 	
+	/**
+	 * Private constructor that produces a successor to a node.
+	 * @param parent   - node to which an action is applied
+	 * @param action   - the action applied to the parent node
+	 * @param strategy - method for calculating the heuristic cost
+	 */
 	private Node(Node<T> parent, T action, Heuristic<T> strategy) {
 		this.parent = parent;
 		this.action = action;
@@ -29,19 +43,36 @@ public class Node<T> implements Comparable<Node<T>> {
 		hcost = strategy.hcost(state);
 	}
 	
+	/**
+	 * Get the state encapsulated by the node.
+	 * @return current state
+	 */
 	public State<T> getState() {
 		return state;
 	}
 
+	/**
+	 * Determines if the current node is at goal state.
+	 * @return true if it is at a goal state
+	 */
 	public boolean isGoalState() {
 		return state.isGoalState();
 	}
 	
+	/**
+	 * Apply the cost function to the node.
+	 * @return node path cost plus heuristic cost
+	 */
 	public int getCost() {
 		return pcost + hcost;
 	}
 	
-	public List<Node<T>> getSuccessor(Heuristic<T> strategy) {
+	/**
+	 * Get the set of all successors to a given node.
+	 * @param strategy - method for calculating the heuristic cost
+	 * @return list of successor nodes
+	 */
+	public List<Node<T>> getSuccessors(Heuristic<T> strategy) {
 		List<Node<T>> successors = new ArrayList<>();
 		
 		for (T curr : state.getValidActions())
@@ -49,6 +80,10 @@ public class Node<T> implements Comparable<Node<T>> {
 		return successors;
 	}
 	
+	/**
+	 * Reconstruct the sequence of actions needed to get to the current node.
+	 * @return list of actions
+	 */
 	public List<T> getActionSequence() {
 		ArrayList<T> sequence = new ArrayList<>();
 		Node<T> curr = this;
@@ -62,7 +97,10 @@ public class Node<T> implements Comparable<Node<T>> {
 		return sequence;
 	}
 	
+	/**
+	 * Compare the total cost of this node with another.
+	 */
 	public int compareTo(Node<T> other) {
-		
+		return Integer.compare(getCost(), other.getCost());
 	}
 }
