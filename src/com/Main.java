1fcb6ff54a9eb2b0a3d532309a7478f9560f68d4
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-14 21:31:21 +1000

    Changed classname back to main since eclipse pathfinding broke

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,11 +3,11 @@ package com;
 import com.GUI.ImageManager;
 import com.GUI.SKBWindow;
 
-public class Game {
+public class Main {
     public static void main(String[] args) {
 
         // load the images
-        ImageManager imgMan= new ImageManager();
+        ImageManager imgMan = new ImageManager();
         imgMan.loadTileImg(Tile.GOAL, "./resources/goal.png");
         imgMan.loadTileImg(Tile.FLOOR, "./resources/floor.png");
         imgMan.loadTileImg(Tile.WALL, "./resources/wall.png");
