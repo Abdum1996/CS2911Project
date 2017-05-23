0d12e025ebb174b5697fa256e40b629b1f9a085a
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-23 15:02:40 +1000

    Wrote some of board generator (is work in progress)

--- src/com/Model/BoardGenerator.java
+++ src/com/Model/BoardGenerator.java
@@ -1,5 +1,10 @@
 package com.Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.List;
+
 /**
  * Factory class responsible for procedurally generating a game board.
  */
@@ -7,29 +12,32 @@ public class BoardGenerator {
 	private static final int MAX_GOALS = 10;
 	
 	public GameBoard genBoard(int width, int height) {
-		/*GameBoard.Builder builder = new SokobanBoard.Builder(width, height);
 		Random generator = new Random();
-		int boardSize = width*height;
 		
-		int numGoals  = generator.nextInt(MAX_GOALS + 1);
-		int numWalls  = generator.nextInt(boardSize - MAX_GOALS + 1);
-		int numFloors = boardSize - numGoals - numWalls;
+		List<Tile> tileList = new ArrayList<>();
+		int tilesLeft = width*height;
+		int numGoals = 0;
 		
-		//for (int i = 0)
-		
-		
-		//ArrayList<Tile> tiles = new ArrayList<>()
-		
-		for (int i = 0; i < MAX_BOXES; ++i) {
-			int offset = generator.nextInt(MAX_BOXES + 1);
-			Point pos = Point.at(offset % width, offset/width);
-			builder.addBox(pos);
+		for (Tile type : Tile.values()) {
+			if (type.equals(Tile.EMPTY)) continue;
+			int numTiles = 0;
+			
+			if (type.equals(Tile.GOAL)) {
+				numTiles = generator.nextInt(MAX_GOALS) + 1;
+				numGoals = numTiles;
+			} else {
+				numTiles = generator.nextInt(tilesLeft) + 1;
+			}
+			
+			for (int i = 0; i < numTiles; ++i)
+				tileList.add(type);
+			tilesLeft -= numTiles;
 		}
 		
+		Collections.shuffle(tileList);
+		GameBoard.Builder builder = new SokobanBoard.Builder(tileList.iterator(), width, height);
 		
 		
-		
-		//Collections.s*/
 		
 		return null;
 	}
