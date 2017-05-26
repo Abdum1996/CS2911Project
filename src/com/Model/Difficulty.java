cc0350239f38b18945d46979a64933aaad56d292
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-26 12:13:58 +1000

    Difficulty enum stores max undos and maximum undo depth

--- src/com/Model/Difficulty.java
+++ src/com/Model/Difficulty.java
@@ -1,12 +1,18 @@
 package com.Model;
 
 public enum Difficulty {
-	EASY(5), NORMAL(0), HARD(0);
+	EASY(5, Integer.MAX_VALUE), NORMAL(3, 3), HARD(0, 0);
 	
+	private final int undoDepth;
 	private final int maxUndos;
 	
-	private Difficulty(int maxUndos) {
-		this.maxUndos = maxUndos;
+	private Difficulty(int undoDepth, int maxUndos) {
+		this.undoDepth = undoDepth;
+		this.maxUndos  = maxUndos;
+	}
+	
+	public int getUndoDepth() {
+		return undoDepth;
 	}
 	
 	public int getMaxUndos() {
