3cfb2e9236062a3d52e2cef58bbc1ded61d36446
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-27 13:00:36 +1000

    simplified sized stack

--- src/com/Model/SizedStack.java
+++ src/com/Model/SizedStack.java
@@ -1,30 +1,61 @@
 package com.Model;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Deque;
 
 /**
- * A limited sized stack that holds the n most recently pushed items as specified by the user
- * @author Abdulrahman Alhomayany
- *
- * @param <T> - The type of items this stack holds
+ * Representation of a stack with a given size limit. If the limit
+ * is exceeded then the bottom most item is removed from the stack.
+ * @param <T> - type of item stored in the stack
  */
-@SuppressWarnings("serial")
-public class SizedStack<T> extends Stack<T> {
+public class SizedStack<T>  {
+	private final Deque<T> deque;
+	private final int limit;
+	
 	/**
-	 * The maximum number of items this stack holds
+	 * Construct a new sized stack with a given limit.
+	 * @param limit - maximum number of items allowed in the stack
+	 * @throws IllegalArgumentException if the limit < 0
 	 */
-    private int limit;
-
-    public SizedStack(int size) {
-        super();
-        this.limit = size;
-    }
-
-    @Override
-    public T push(T object) {
-        while (this.size() >= limit) {
-            this.remove(0);
-        }
-        return super.push(object);
-    }
+	public SizedStack(int limit) {
+		if (limit < 0) throw new IllegalArgumentException("Stack limit must be >= 0");
+		
+		deque = new LinkedList<>();
+		this.limit = limit;
+	}
+	
+	/**
+	 * Push an item onto the sized stack.
+	 * @param item - item to be pushed
+	 */
+	public void push(T item) {
+		deque.push(item);
+		
+		if (deque.size() > limit)
+			deque.removeLast();
+		return;
+	}
+	
+	/**
+	 * Pop an item from the sized stack stack.
+	 * @return item removed from the stack
+	 */
+	public T pop() {
+		return deque.pop();
+	}
+	
+	/**
+	 * Clear the sized stack of its contents.
+	 */
+	public void clear() {
+		deque.clear();
+	}
+	
+	/**
+	 * Determine if the sized stack is empty.
+	 * @return true if it is empty
+	 */
+	public boolean isEmpty() {
+		return deque.isEmpty();
+	}
 }
