243ac3b50ff98e56ee1f00dba63b07bb3b2d297b
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-28 14:58:02 +1000

    BoardState has a new constructor

--- src/com/Model/BoardGeneratorOld.java
+++ src/com/Model/BoardGeneratorOld.java
@@ -1,142 +1,81 @@
 package com.Model;
 
-import java.util.NoSuchElementException;
 import java.io.FileNotFoundException;
 
 import java.io.FileReader;
 import java.util.Scanner;
-
-import java.util.Collections;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
- * Factory class responsible for creating new game boards, both by reading
- * input map files and through random procedural generation methods.
+ * Factory class responsible for creating new tile maps,
+ * through random procedural generation methods.
  * @author Samir Mustavi
  * @author Thomas Daniell
  */
-public class BoardGenerator {
+public class BoardGeneratorOld {
 	private static final Random generator = new Random();
 	private static final int MAX_GOALS = 10;
 	private Template[] templates;
 	
 	public BoardGenerator() {
-		int i;
-		for (i = 0; i < 17; i++) {
+		templates = new Template[17];
+		
+		for (int i = 0; i < 17; i++) {
 			templates[i] = new Template(getArray(i+1));
 		}
 	}
 	
-	/**
-	 * Generate a Sokoban board from an input map file.
-	 * @param filename - name of text file
-	 * @return the new Sokoban board
-	 */
-	public static SokobanBoard readMap(String filename) {
-		SokobanBoard board = null;
-		Scanner sc = null;
+	public static TileMap genTileCollection() {
+		BoardGenerator gen = new BoardGenerator();
+		char[][] emptyBoard = gen.emptyBoard(GameConstants.MAX_MAP_HEIGHT, 
+				GameConstants.MAX_MAP_WIDTH);
 		
-		try {
-			sc = new Scanner(new FileReader(filename));
-			
-			int width = sc.nextInt();
-			int height = sc.nextInt();
-			
-			List<Tile> tiles = new ArrayList<>(width*height);
-			List<Point> boxPositions = new ArrayList<>();
-			Point playerPos = Point.at(0, 0);
-			
-			for (int y = 0; y < height; ++y) {
-				for (int x = 0; x < width; ++x) {
-					String symbol = sc.next();
-					tiles.add(Tile.parse(symbol));
-					
-					Point pos = Point.at(x, y);
-					if (symbol.equals("P")) {
-						playerPos = pos;
-					} else if (symbol.equals("B")) {
-						boxPositions.add(pos);
-					}
+		
+		List<Point> floorList = new ArrayList<>();
+		for (int i = 0; i < GameConstants.MAX_MAP_HEIGHT; ++i) {
+			for (int j = 0; j < GameConstants.MAX_MAP_WIDTH; ++j) {
+				if (emptyBoard[i][j] == 'F') {
+					floorList.add(Point.at(j, i));
 				}
 			}
-			
-			TileMap map = new TileMap(tiles.iterator(), width, height);
-			board = new SokobanBoard(map, boxPositions.iterator(), 
-					playerPos, width, height);
-			
-		} catch (FileNotFoundException | NoSuchElementException e) {
-			e.printStackTrace();
-			
-		} finally {
-			if (sc != null) sc.close();
 		}
-		
-		return board;
-	}
-	
-	/**
-	 * Procedurally generate a new Sokoban board.
-	 * @param width  - width of the board in columns 
-	 * @param height - height of the board in rows
-	 * @return procedurally generated game board
-	 */
-	public static SokobanBoard genBoard(int width, int height) {
-		List<Tile> tiles = new ArrayList<>(width*height);
 		
 		int numGoals = generator.nextInt(MAX_GOALS) + 1;
-		int tilesLeft = width*height - numGoals;
-		
-		for (int i = 0; i < numGoals; ++i)
-			tiles.add(Tile.GOAL);
-		
-		for (Tile type : Tile.values()) {
-			if (type.equals(Tile.EMPTY) || type.equals(Tile.GOAL)) continue;
-			int numTiles = generator.nextInt(tilesLeft) + 1;
-			tilesLeft -= numTiles;
+		Collections.shuffle(floorList);
 			
-			for (int i = 0; i < numTiles; ++i)
-				tiles.add(type);
+		for (int i = 0; i < numGoals; ++i) {
+			Point pos = floorList.get(i);
+			emptyBoard[pos.getY()][pos.getX()] = 'G';
 		}
 		
-		Collections.shuffle(tiles);
-		TileMap map = new TileMap(tiles.iterator(), width, height); 
-		
-		SokobanBoard board = null;
-		do {
-			List<Point> boxPositions = new ArrayList<>();
-			
-			for (int i = 0; i < numGoals; ++i) {
-				Point boxPos = genRandomPoint(map);
+		List<Tile> tiles = new ArrayList<>();
+		for (int i = 0; i < GameConstants.MAX_MAP_HEIGHT; ++i) {
+			for (int j = 0; j < GameConstants.MAX_MAP_WIDTH; ++j) {
+				char symbol = emptyBoard[i][j];
+				Tile tile;
 				
-				while (!map.isValidEntityPos(boxPos))
-					boxPos = genRandomPoint(map);
-					boxPositions.add(boxPos);
+				switch (symbol) {
+					case 'E':
+					case 'W':
+						tile = Tile.WALL;
+					case 'G':
+						tile = Tile.GOAL;
+					case 'F':
+						tile = Tile.FLOOR;
+					default:
+						tile = Tile.EMPTY;	
+				}
+				
+				tiles.add(tile);
 			}
-			
-			Point playerPos = genRandomPoint(map);
-			while (!map.isValidEntityPos(playerPos)) {
-				playerPos = genRandomPoint(map);
-			}
-			
-			board = new SokobanBoard(map, boxPositions.iterator(),
-					playerPos, width, height);
-		} while (!board.isSolvable());
-		//Collections.s*/
+		}
 		
-		return board;
-	}
-	
-	/**
-	 * Generates a random point from a given map, for placing boxes and players
-	 * @param map - the map to generate a random point for
-	 * @return a random point
-	 */
-	private static Point genRandomPoint(TileMap map) {
-		int offset = generator.nextInt(map.getHeight()*map.getHeight());
-		return Point.at(offset % map.getWidth(), offset/map.getHeight());
+		TileMap map = new TileMap(tiles, GameConstants.MAX_MAP_WIDTH, 
+				GameConstants.MAX_MAP_HEIGHT);
+		return map;
 	}
 	
 	/**
@@ -147,20 +86,26 @@ public class BoardGenerator {
 	 */
 	public char[][] emptyBoard(int height, int width) {
 		char[][] board = new char[height][width];
-		Arrays.fill(board,'E');
+		for (int i = 0; i < height; ++i) {
+			for (int j = 0; j < width; ++j) {
+				board[i][j] = 'E';
+			}
+		}
+		
 		int i = 2;
 		int j = 2;
 		char[][] tempArray = null;
 		Template t = null;
 		
-		while (i < height) {
+		while (i < height - 4) {
 			t = templates[generator.nextInt(17)];
 			t.modifyTemplate(generator);
+			
 			tempArray = t.getTemplateMap();
 			if (!properOverlap(board,tempArray,i,j)) continue;
 			board = tempToBoard(board,tempArray,i,j);
 			j += 3;
-			if (j < width) continue;
+			if (j < width - 4) continue;
 			j = 2;
 			i += 3;
 		}
@@ -177,7 +122,7 @@ public class BoardGenerator {
 	 * @param j - the horizontal index of the board to place the template
 	 * @return true if the template can be placed on the board with no conflicting overlaps, false otherwise
 	 */
-	private boolean properOverlap(char[][] array, char[][] t, int i, int j) {
+	private static boolean properOverlap(char[][] array, char[][] t, int i, int j) {
 		int x1 = 0;
 		int y1 = 0;
 		int x2;
@@ -185,9 +130,12 @@ public class BoardGenerator {
 		for (x2 = i-1; x2 < i+4; x2++) {
 			for (y2 = j-1; y2 < j+4; y2++) {
 				if ((t[x1][y1] != 'E') && (array[x2][y2] != 'E')) {
-					if (t[x1][x2] != array[x2][y2]) return false;
+					if (t[x1][y1] != array[x2][y2]) return false;
 				}
+				y1++;
 			}
+			y1 = 0;
+			x1++;
 		}
 		return true;
 	}
@@ -200,7 +148,7 @@ public class BoardGenerator {
 	 * @param j - the horizontal index of the board to place the template
 	 * @return the same board with the template added on
 	 */
-	private char[][] tempToBoard(char[][] array, char[][] t, int i, int j) {
+	private static char[][] tempToBoard(char[][] array, char[][] t, int i, int j) {
 		int x1 = 0;
 		int y1 = 0;
 		int x2;
@@ -208,7 +156,10 @@ public class BoardGenerator {
 		for (x2 = i-1; x2 < i+4; x2++) {
 			for (y2 = j-1; y2 < j+4; y2++) {
 				if ((t[x1][y1] != 'E') && (array[x2][y2] == 'E')) array[x2][y2] = t[x1][y1];
+				y1++;
 			}
+			y1 = 0;
+			x1++;
 		}
 		return array;
 	}
@@ -222,15 +173,14 @@ public class BoardGenerator {
 		String s = "./resources/Templates/t" + index + ".txt";
 		Scanner sc = null;
 	    char array[][] = new char[5][5];
-	    try
-	    {
+	    
+	    try {
 	        sc = new Scanner(new FileReader(s));
 		    String curr = null;
 		    String[] c = null;
 		    int i,j = 0;
 		    
-		    while ((sc.hasNextLine()) || (j < 5)) {
-		    
+		    while ((sc.hasNextLine()) || (j < 5)) {    
 		    	curr = sc.nextLine();
 		    	c = curr.split(" ");
 		    	
@@ -240,14 +190,12 @@ public class BoardGenerator {
 		    	j++;
 		    }
 
-	    }
-	    catch (FileNotFoundException e) {}
-
-	    finally
-	    {
+	    } catch (FileNotFoundException e) {
+	    	// do nothing
+	    } finally {
 	        if (sc != null) sc.close();
-
-	    } 
+	    }
+	    
 		return array;
 	}
 	
