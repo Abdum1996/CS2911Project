3ca8f8e82c47300a5a4246d2fe227b3b2b2c870f
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-28 21:59:57 +1000

    Updated heuristic

--- src/com/Model/BoardHeuristic.java
+++ src/com/Model/BoardHeuristic.java
@@ -14,12 +14,28 @@ public class BoardHeuristic implements Heuristic<Direction> {
 		Point playerPos = trueState.getPlayerPos();
 		TileMap map = trueState.getTileMap();
 		
-		// Determine if any boxes are in 'dead lock' positions
-		for (Point curr : boxPositions) {
-			int validCount = map.getAdjTilePoints(curr).size();
-			if (!map.get(curr).equals(Tile.GOAL) && validCount <= 2) return Integer.MAX_VALUE;	
+		int totalCost = 0;
+		for (Point boxPos : boxPositions) {
+			// Boxes already in goal states obviously can be ignored
+			if (map.get(boxPos).equals(Tile.GOAL)) continue;
+			
+			// Ensure that the box is not in a dead lock
+			int validCount = map.getAdjTilePoints(boxPos).size();
+			if (validCount <= 2) return Integer.MAX_VALUE;
+			
+			// Calculate the shortest distance between each box and their closest goal
+			int minDist = Integer.MAX_VALUE;
+			for (Point goalPos : map.getGoalPositions()) {
+				if (boxPositions.contains(goalPos)) continue;
+				
+				int currDist = map.getShortestDist(boxPos, goalPos);
+				if (currDist < minDist)
+					minDist = currDist;
+			}
+			
+			totalCost += minDist;
 		}
 		
-		return 0;
+		return totalCost;
 	}
 }
