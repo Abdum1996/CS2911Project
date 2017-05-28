c3f25d33b7727d0f36687296d968beb327463025
Thomas Daniell <thomas.tomdan@icloud.com>
2017-05-28 22:00:14 +1000

    Deleted redundant code in model generator

--- src/com/Model/BoardGenerator.java
+++ src/com/Model/BoardGenerator.java
@@ -4,9 +4,7 @@ import java.util.Random;
 
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.Graph.AStarSearch;
 import com.Graph.Heuristic;
@@ -14,10 +12,6 @@ import com.Graph.Heuristic;
 public class BoardGenerator {
 	private static final Random generator = new Random();
 	private static final int MAX_GOALS = 5;
-	
-	private static final int WIDTH  = 20;
-	private static final int HEIGHT = 20;
-	private static final int SIZE   = WIDTH*HEIGHT;
 	
 	private final List<Point> boxPositions;
 	private final int numGoals;
@@ -52,10 +46,10 @@ public class BoardGenerator {
 		AStarSearch<Direction> space;
 		
 		do {
-			tileMap = LayoutGenerator.genTileMap(numGoals);
-		} while (!tileMap.isPathConnected());
-		
-		do {
+			do {
+				tileMap = LayoutGenerator.genTileMap(numGoals);
+			} while (!tileMap.isPathConnected());
+			
 			System.out.println("loading");
 			placeEntities();
 			space = getSearchSpace();
@@ -68,29 +62,6 @@ public class BoardGenerator {
 		BoardState start = new BoardState(tileMap, playerPos, boxPositions);
 		Heuristic<Direction> heuristic = new BoardHeuristic();
 		return new AStarSearch<>(heuristic, start);
-	}
-	
-	private TileMap genTileMap() {
-		List<Tile> newTiles = new ArrayList<>(SIZE);
-		Map<Tile, Integer> amounts = new HashMap<>();
-		
-		int minFloors = numGoals + 1;
-		int bound = SIZE - numGoals - minFloors;
-		int numFloors = generator.nextInt(bound/2) + minFloors + 1; // Reduce floors placed
-		int numWalls = SIZE - numGoals - numFloors;
-		
-		amounts.put(Tile.GOAL, numGoals);
-		amounts.put(Tile.FLOOR, numFloors);
-		amounts.put(Tile.WALL, numWalls);
-		
-		for (Tile type : amounts.keySet()) {
-			int numTiles = amounts.get(type);
-			for (int i = 0; i < numTiles; ++i)
-				newTiles.add(type);
-		}
-		
-		Collections.shuffle(newTiles);
-		return new TileMap(newTiles, WIDTH, HEIGHT);
 	}
 	
 	private void placeEntities() {
